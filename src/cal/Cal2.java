ebbf54e2b6612525828aa56ea62b0e9091412c7c
hayab <hayab@DESKTOP-1SDLGOS>
2018-01-07 18:58:14 +0900

    scanner and static

--- src/cal/Cal2.java
+++ src/cal/Cal2.java
@@ -17,11 +17,23 @@ public class Cal2 {
 	 * -1을 입력받기 전까지 반복 입력 받는다.
 	 * 프롬프트를 출력한다.
 	 * */
-	
-	
-	
+		
 	public static void main(String[] args) {
 		
+		System.out.println("해당월을 입력하세요.");
+		Scanner sc = new Scanner(System.in);
+		
+		String month=sc.next();
+       //int lastday[]= {31,28,31,30,31,30,31,31,30,31,30,31};
+		int month2 =Integer.parseInt(month);
+		
+		//System.out.printf("%d월은 %d일까지 있습니다.", month2, lastday[month2-1]);
+		
+		Cal2 c = new Cal2();
+		c.getMaxDaysOfMonth(month2);
+		System.out.printf("%d월은 %d일까지 있습니다.", month2,c.getMaxDaysOfMonth(month2) );
+		
+		sc.close();
 	}
 
 }
