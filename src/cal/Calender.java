cc700bdb8ca267b1ad0e2bef52d82a3df51484e1
hayab <hayab@DESKTOP-1SDLGOS>
2018-01-07 18:08:21 +0900

    commit 2

--- src/cal/Calender.java
+++ src/cal/Calender.java
@@ -2,11 +2,19 @@ package cal;
 
 //깃허브 저장하기
 
+// 1. 깃허브 로그인 - > 저장소 만든 후
+
+//2. 이클립스 프로젝트 후 Team -> share ->  
+
+
 public class Calender {
 
 	public static void main(String[] args) {
 		System.out.println("hello");
-		// Add to Index 하고 커밋  -> 로컬에 저장
+		// 3. Add to Index 하고 커밋  -> 로컬에 저장
 		// push 하면 원격에 저장
+		//4. 프로젝트에서 우클릭 team-> push branch master
+		
+		System.out.println("hello2");
 	}
 }
