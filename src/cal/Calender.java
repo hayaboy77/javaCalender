c525c9025e226247da32d26ea7d8130344677f9e
hayab <hayab@DESKTOP-1SDLGOS>
2018-01-07 17:55:25 +0900

    init calendar project

---
+++ src/cal/Calender.java
@@ -0,0 +1,12 @@
+package cal;
+
+//깃허브 저장하기
+
+public class Calender {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		// Add to Index 하고 커밋  -> 로컬에 저장
+		// push 하면 원격에 저장
+	}
+}
